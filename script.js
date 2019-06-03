3f7005e63c89c1e3e65fb56dba304bec02639aab
Peter Mackay <project2501@gmail.com>
2019-06-03 08:03:13 -0600

    all basic functions implemented

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-  var cards = [{title:"Card One", body:"blahblahblah"}, {title:"Card Two",body:"harharhar"}];
-  var forwardButton = $('#cardAdvanceButton');
+  var cards = [{title:"Ilithid", body:"Psionic tyrants, slavers, and interdimensional voyagers, they are insidious masterminds that harvest entire races for their own twisted ends. Four tentacles snake from their octopus-like heads, flexing in hungry anticipation when sentient creatures come near."}, 
+               {title:"Shambling Mound",body:"A shambling mound, sometimes called a shambler, trudges ponderously through bleak swamps, dismal marshes, and rain forests, consuming any organic matter in its path. This rotting heap of animated vegetation looms up half again as tall as a human, tapering into a faceless head at its top."},
+                {title:"Demilich",body:"The immortality granted to a lich lasts only as long as it feeds mortal souls to its phylactery. If it falters or fails in that task, its bones turn to dust until only its skull remains. This demilich contains only a fragment of the lich’s malevolent life force — just enough so that if it is disturbed, these remains rise into the air and assume a wraithlike form. The skull then emits a terrifying howl that can slay the weak-hearted and leave others trembling with fear. Left alone, it sinks back down and returns to the empty peace of its existence."}
+              ];
+  
+               var forwardButton = $('#cardAdvanceButton');
   var backButton = $('#cardRetreatButton');
   var addButton = $('#addCardButton')
   var deleteButton = $('#deleteCardButton')
   var currentCard = 0;
 
-  var titleField = $('card_title')
-  var bodyField = $('card_body')
+  var titleField = $('#card_title')
+  var bodyField = $('#card_body')
 
 // start with the first card
   updateCardDisplay(0)
@@ -62,17 +66,27 @@ function addCard(){
   updateCardDisplay(currentCard)
 }
 
-// make the title editable
+
+
+titleField.on('click', function(){
   var content = document.querySelector('#card_title');
   content.addEventListener('input', function(event) {
     cards[currentCard].title = content.innerHTML;
   })
+})
 
+
+bodyField.on('click', function(){
   // make the body editiable
   var content = document.querySelector('#card_body');
   content.addEventListener('input', function(event) {
     cards[currentCard].body = content.innerHTML;
   })
+})
+
+
+
+
 
 })
 
